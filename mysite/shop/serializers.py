045a79a04e585dab05184fe2deaa63c4741c72cc
User <sheparov.71@mail.ru>
2023-06-12 19:26:33 +0300

    fix category images serializer

--- mysite/shop/serializers.py
+++ mysite/shop/serializers.py
@@ -1,18 +1,31 @@
 from rest_framework import serializers
-
+from rest_framework.fields import SerializerMethodField
 
 from .models import *
 
 
 class SubCategorySerializer(serializers.ModelSerializer):
+    image = SerializerMethodField()
     class Meta:
         model = SubCategory
         fields = ["id", "title", "image"]
 
+    def get_image(self, obj):
+        return {
+            "src": obj.image.url,
+            "alt": ""
+        }
+
 
 class CategorySerializer(serializers.ModelSerializer):
     subcategories = SubCategorySerializer(many=True, read_only=True)
-
+    image = SerializerMethodField()
     class Meta:
         model = Category
         fields = ["id", "title", "image", "subcategories"]
+
+    def get_image(self, obj):
+        return {
+            "src": obj.image.url,
+            "alt": ""
+        }
